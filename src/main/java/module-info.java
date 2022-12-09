18dfaa0407af562364e780fe234f9a3c061500cd
littie1987 <littie1987@sina.com>
2022-12-09 10:37:54 +0800

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.xm.dbms {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.xm.dbms to javafx.fxml;
+    exports com.xm.dbms;
+}
